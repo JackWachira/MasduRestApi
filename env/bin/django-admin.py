6bb339298bfb2c82d30833c2bb509b1b38004bf3
“JackMwangi” <jackmwa94@gmail.com>
2016-06-07 17:00:27 +0300

    (Chore) Add gunicorn

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/JackMwangi/Documents/Python_Workspace/Checkpoint3/Django-Powered-BucketList-Application/Bucketlist_backend/env/bin/python2.7
+#!/Users/JackMwangi/Documents/Python_Workspace/Checkpoint3/MasduRestApi/env/bin/python2.7
 from django.core import management
 
 if __name__ == "__main__":
