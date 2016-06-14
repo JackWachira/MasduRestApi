99015593df61975ca1bfe54b2bcbfefc5fa700b3
“JackMwangi” <jackmwa94@gmail.com>
2016-06-14 17:19:00 +0300

    (Chore) remove gzip

--- MasduRestApi/settings/base.py
+++ MasduRestApi/settings/base.py
@@ -190,8 +190,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
