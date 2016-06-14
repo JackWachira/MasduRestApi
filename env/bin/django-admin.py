1614920e864a42610c4d50d17c84be23e513abd8
“JackMwangi” <jackmwa94@gmail.com>
2016-06-14 13:13:06 +0300

    (Chore) Remove env from tracking

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/JackMwangi/Documents/Python_Workspace/Checkpoint3/MasduRestApi/env/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
